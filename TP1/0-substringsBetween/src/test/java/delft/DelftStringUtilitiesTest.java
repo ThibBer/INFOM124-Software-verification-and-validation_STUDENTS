4e4838a55441e031cc6d040beeeb9fa0536f9a41
ThibBer <thibaut.berg@hotmail.com>
2023-10-24 11:30:54 +0200

    TP1

--- TP1/0-substringsBetween/src/test/java/delft/DelftStringUtilitiesTest.java
+++ TP1/0-substringsBetween/src/test/java/delft/DelftStringUtilitiesTest.java
@@ -1,13 +1,25 @@
 package delft;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.*;
 import java.util.stream.*;
-import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
 
+
 class DelftStringUtilitiesTest {
+    @Test
+    void nullStr(){
+        // Arrange
+        final String str = null;
+        final String open = "";
+        final String close = "";
+        // Act
+        String[] substrings = delft.DelftStringUtilities.substringsBetween(str, open, close);
+        // Assert
+        assertEquals(null, substrings);
+    }
 }
