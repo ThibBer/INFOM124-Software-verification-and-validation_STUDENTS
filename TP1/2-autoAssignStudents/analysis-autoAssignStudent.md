cafb53e597435ade3a4611368a18d72e0d88aa1e
ThibBer <thibaut.berg@hotmail.com>
2023-10-25 15:30:58 +0200

    AutoAssign specification & test

---
+++ TP1/2-autoAssignStudents/analysis-autoAssignStudent.md
@@ -0,0 +1,27 @@
+# Specification-based Testing - AutoAssignStudents
+
+## 1. Goal, inputs and outputs
+- Goal: Assign students to workshops
+- Input domain: A list of students & a list of workshops
+- Output domain: An AssignmentsLogger object which contains assignments & errors
+
+## 2. Explore the program
+
+## 3. Identify input and output partition
+### Input partitions
+#### Individual inputs
+Workshop avec assez de place
+    - Même date
+    - Date différente
+Workshop pas assez de place
+Pas d'étudiants
+Pas de workshop
+
+#### Combinations of input values
+
+
+### Output partitions
+
+## 4. Identify boundaries
+
+## 5. Select test cases
--- TP1/2-autoAssignStudents/src/test/java/delft/AutoAssignerTest.java
+++ TP1/2-autoAssignStudents/src/test/java/delft/AutoAssignerTest.java
@@ -12,13 +12,112 @@ import org.junit.jupiter.params.provider.*;
 import java.time.*;
 
 class AutoAssignerTest {
+    private AutoAssigner autoAssigner;
 
-
+    @BeforeEach
+    private void init(){
+        autoAssigner = new AutoAssigner();
+    }
 
     // helper method to create a ZonedDateTime
     private ZonedDateTime date(int year, int month, int day, int hour, int minute) {
         return ZonedDateTime.of(year, month, day, hour, minute, 0, 0, ZoneId.systemDefault());
     }
 
+    @Test
+    void noWorkshop(){
+        // Assign
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Name", "Email"));
 
+        // Act
+        AssignmentsLogger assignmentsLogger = autoAssigner.assign(students, new ArrayList<>());
+
+        // Assert
+        assertThat(assignmentsLogger.getAssignments()).isEmpty();
+        assertThat(assignmentsLogger.getErrors()).isEmpty();
+    }
+
+    @Test
+    void noStudent(){
+        // Assign
+        ArrayList<Workshop> workshops = new ArrayList<>();
+        workshops.add(new Workshop(1, "Name", Map.of(
+            date(2023, 10, 25, 15, 20), 1,
+            date(2023, 11, 26, 16, 30), 2
+        )));
+
+        // Act
+        AssignmentsLogger assignmentsLogger = autoAssigner.assign(new ArrayList<>(), workshops);
+
+        // Assign
+        assertThat(assignmentsLogger.getAssignments()).isEmpty();
+        assertThat(assignmentsLogger.getErrors()).isEmpty();
+    }
+
+    @Test
+    void enoughPlacesInWorkshopSameDate(){
+        // Assign
+        ArrayList<Workshop> workshops = new ArrayList<>();
+        workshops.add(new Workshop(1, "Name", Map.of(
+            date(2023, 10, 25, 15, 20), 5
+        )));
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Name", "Email"));
+        students.add(new Student(2, "Name 2", "Email 2"));
+        students.add(new Student(3, "Name 3", "Email 3"));
+
+        // Act
+        AssignmentsLogger assignmentsLogger = autoAssigner.assign(students, workshops);
+
+        // Assign
+        assertThat(assignmentsLogger.getAssignments()).hasSize(3);
+        assertThat(assignmentsLogger.getErrors()).hasSize(0);
+    }
+
+    @Test
+    void enoughPlacesInWorkshopDifferentDates(){
+        // Assign
+        ArrayList<Workshop> workshops = new ArrayList<>();
+        workshops.add(new Workshop(1, "Name", Map.of(
+            date(2023, 10, 25, 15, 20), 1,
+            date(2023, 4, 14, 9, 5), 1,
+            date(2023, 8, 10, 12, 10), 1
+        )));
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Name", "Email"));
+        students.add(new Student(2, "Name 2", "Email 2"));
+        students.add(new Student(3, "Name 3", "Email 3"));
+
+        // Act
+        AssignmentsLogger assignmentsLogger = autoAssigner.assign(students, workshops);
+
+        // Assign
+        assertThat(assignmentsLogger.getAssignments()).hasSize(3);
+        assertThat(assignmentsLogger.getErrors()).hasSize(0);
+    }
+
+    @Test
+    void notEnoughPlacesInWorkshop(){
+        // Assign
+        AutoAssigner autoAssigner = new AutoAssigner();
+        ArrayList<Workshop> workshops = new ArrayList<>();
+        workshops.add(new Workshop(1, "Name", Map.of(
+            date(2023, 10, 25, 15, 20), 1
+        )));
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Name", "Email"));
+        students.add(new Student(2, "Name 2", "Email 2"));
+        students.add(new Student(3, "Name 3", "Email 3"));
+
+        // Act
+        AssignmentsLogger assignmentsLogger = autoAssigner.assign(students, workshops);
+
+        // Assign
+        assertThat(assignmentsLogger.getAssignments()).hasSize(1);
+        assertThat(assignmentsLogger.getErrors()).hasSize(2);
+    }
 }
